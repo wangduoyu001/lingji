0db1aeda0d66cd7c3c8b431ebfdb71cc467776ba
wangduoyu001 <131972922+wangduoyu001@users.noreply.github.com>
2026-07-19 03:56:19 +0800

    feat: add single-vault memory package

---
+++ src/memory/__init__.py
@@ -0,0 +1,4 @@
+from .capture import InboxService
+from .vault_layout import LAYOUT_VERSION, VaultClassification, VaultLayout
+
+__all__ = ["InboxService", "LAYOUT_VERSION", "VaultClassification", "VaultLayout"]
