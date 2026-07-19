c1c499a23db557de00fcef2d1445cb491eac1cce
wangduoyu001 <131972922+wangduoyu001@users.noreply.github.com>
2026-07-19 20:29:39 +0800

    feat: export storage lifecycle and backup services

--- src/storage/__init__.py
+++ src/storage/__init__.py
@@ -1,3 +1,10 @@
+from .backup import BackupManager
+from .lifecycle import StorageCategory, StorageLifecycleManager
 from .state_db import StateDatabase
 
-__all__ = ["StateDatabase"]
+__all__ = [
+    "BackupManager",
+    "StateDatabase",
+    "StorageCategory",
+    "StorageLifecycleManager",
+]
