1933cd66b32450c2f89b4fce7d8b4c2cc5c1772f
wangduoyu001 <131972922+wangduoyu001@users.noreply.github.com>
2026-07-29 14:31:22 +0800

    style(ui): load guided usage styles

--- desktop/lingji-control/src/App.tsx
+++ desktop/lingji-control/src/App.tsx
@@ -3,6 +3,7 @@ import AppPages from "./AppPages";
 import DesktopShell from "./components/DesktopShell";
 import RuntimeBoundary from "./components/RuntimeBoundary";
 import "./DesktopUX.css";
+import "./GuidedUsage.css";
 import "./ReleaseUX.css";
 import { useLingJiConnection } from "./hooks/useLingJiConnection";
 import { useReleaseMetadata } from "./hooks/useReleaseMetadata";
